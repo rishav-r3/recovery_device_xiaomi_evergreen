eec76765c35211d9eaac2426aa399f623fba4b00
Albert I <kras@raphielgang.org>
2022-10-28 12:15:54 +0530

    evergreen: Define brightness-related flags for TWRP
      Saving your time for recursive search of sysfs :)
    
      Signed-off-by: Albert I <kras@raphielgang.org>
      Signed-off-by: Rishav Raj <rishavraj1892@gmail.com>
      Change-Id: I6eab8dca82f36cfea6af6f40073c6f4981b78cbf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,3 +111,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
+
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+TW_MAX_BRIGHTNESS := 4095 
+TW_DEFAULT_BRIGHTNESS := 2048 
